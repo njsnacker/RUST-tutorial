3dc72cc49bdfb9b929a7ccab4b8eb7dd62b3373c
njsnacker <njsnacker@gmail.com>
2024-10-28 18:42:27 +0900

    add serial thread example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::thread;
+use std::time::Duration;
+
 fn main() -> eframe::Result {
+    thread::spawn(|| {
+        let ports = serialport::available_ports().expect("No ports found!");
+
+        let mut port = serialport::new("/dev/ttyUSB0", 9_600)
+            .timeout(Duration::from_millis(1000))
+            .open()
+            .expect("Failed to open port");
+
+        for p in ports {
+            println!("{}", p.port_name)
+        }
+
+        loop {
+            let mut serial_buf: Vec<u8> = vec![0; 32];
+            port.read(serial_buf.as_mut_slice())
+                .expect("Found no data!");
+            println!(
+                "Buf {}",
+                serial_buf
+                    .iter()
+                    .map(|b| format!("{:02x}", b).to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            );
+            serial_buf.clear();
+        }
+        for i in 1..10 {
+            println!("num {i} form spawned thread!");
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
