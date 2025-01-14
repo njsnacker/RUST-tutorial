90c837d7e9e00cec8c6d82a5cb8c80ea499d9796
njsnacker <njsnacker@gmail.com>
2025-01-14 17:26:56 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,53 @@
-#![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
-use std::thread;
 use std::time::Duration;
 
-fn main() -> eframe::Result {
-    thread::spawn(|| {
-        let ports = serialport::available_ports().expect("No ports found!");
+use serde::ser::SerializeStructVariant;
+use serialport::{SerialPortInfo, SerialPortType};
 
-        let mut port = serialport::new("/dev/ttyUSB0", 9_600)
-            .timeout(Duration::from_millis(1000))
-            .open()
-            .expect("Failed to open port");
+fn scan_serial_ports() -> Vec<String> {
+    let debug_ports = true;
+    let mut ports_list = Vec::new();
 
-        for p in ports {
-            println!("{}", p.port_name)
+    match serialport::available_ports() {
+        Ok(ports) => {
+            if ports.is_empty() {
+                print!("No ports exists");
+            } else {
+                for port in ports {
+                    match port.port_type {
+                        SerialPortType::UsbPort(usb_port_info) => {
+                            ports_list.push(port.port_name.clone());
+
+                            if debug_ports {
+                                println!("포트 이름 : {}", port.port_name);
+                                if let Some(manufacturer) = usb_port_info.manufacturer {
+                                    println!("제조사: {manufacturer}");
+                                }
+                                if let Some(product) = usb_port_info.product {
+                                    println!("제품명: {product}");
+                                }
+                                if let Some(serial_number) = usb_port_info.serial_number {
+                                    println!("시리얼 번호: {serial_number}\n");
+                                }
+                            }
+                        }
+                        SerialPortType::PciPort => {}
+                        SerialPortType::BluetoothPort => {}
+                        SerialPortType::Unknown => {}
+                    }
+                }
+            }
         }
-
-        loop {
-            let mut serial_buf: Vec<u8> = vec![0; 32];
-            port.read(serial_buf.as_mut_slice())
-                .expect("Found no data!");
-            println!(
-                "Buf {}",
-                serial_buf
-                    .iter()
-                    .map(|b| format!("{:02x}", b).to_string())
-                    .collect::<Vec<String>>()
-                    .join(" ")
-            );
-            serial_buf.clear();
+        Err(e) => {
+            // ports_info.push(format!("포트 검색 중 에러 발생: {}", e));
+            print!("Error when scanning ports {e}");
         }
-        for i in 1..10 {
-            println!("num {i} form spawned thread!");
-            thread::sleep(Duration::from_millis(1000));
-        }
-    });
+    }
 
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    return ports_list;
+}
 
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-                    .expect("Failed to load icon"),
-            ),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Serial Tool",
-        native_options,
-        Box::new(|cc| Ok(Box::new(RUST_tutorial::SerialApp::new(cc)))),
-    )
+fn main() {
+    let mut serial_buf: Vec<u8> = vec![0; 32];
+
+    scan_serial_ports();
 }
